4d4921a94689fc044971473388a2a8928d863c90
arjun-ux <arux899@gmail.com>
2023-08-17 06:13:16 +0700

    lagi

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,6 +3,5 @@
 @section('konten')
     <a href="#">User</a>
     <a href="/barang">Daftar Barang</a>
-    <a href="{{ route('logout') }}">logout</a>
     <h1>halaman dashboard</h1>
 @endsection
--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar" id="navmain">
-  <div class="container">
+  <div class="container-fluid">
     <a class="navbar-brand fw-bold" href="/">
       Teddy's Mart
     </a>
+    <a href="{{ route('logout') }}">Logout</a>
   </div>
 </nav>
