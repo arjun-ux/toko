401fb01dae1720d456973519205cff228f428762
unknown <arux899@gmail.com>
2023-09-13 19:55:04 +0700

    mencoba responsive

--- resources/views/dashboard/admin/userProfile.blade.php
+++ resources/views/dashboard/admin/userProfile.blade.php
@@ -3,25 +3,25 @@
 @section('konten')
     
     <div class="container-fluid p-0">
-    <div class="shadow p-3 mb-5 bg-body rounded fw-bold">
+    <div class="shadow p-3 mb-4 bg-body rounded fw-bold">
        My Profile
     </div>
         <div class="row">
-            <div class="col-md-3 text-center">
+            <div class="col-md-3 text-center mb-2">
                 <img class="img-thumbnail rounded" src="img/pondo.png" alt="Foto Admin">
             </div>
-            <div class="col-md-auto">
 
-                <div class="row">
+            <div class="col-md-auto">
+                <div class="row mb-2">
                     <label for="name" class="col-sm-3 col-form-label">Nama</label>
                     <div class="col-sm-9">
-                    <input type="text" readonly class="form-control-plaintext" id="name" value=": {{ Auth::user()->name }}" >
+                    <input type="text" readonly class="form-control" id="name" value=": {{ Auth::user()->name }}" >
                     </div>
                 </div>
-                <div class="row">
+                <div class="row mb-2">
                     <label for="email" class="col-sm-3 col-form-label">Email</label>
                     <div class="col-sm-9">
-                    <input type="text" readonly class="form-control-plaintext" id="email" value=": {{ Auth::user()->email }}" >
+                    <input type="text" readonly class="form-control" id="email" value=": {{ Auth::user()->email }}" >
                     </div>
                 </div>
             </div>
