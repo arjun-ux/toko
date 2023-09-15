3b3a8e5154097db22b82a30313f4cf9a5a9dc8c5
arjun-ux <arux899@gmail.com>
2023-09-15 16:58:12 +0700

    ukuran img

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -19,9 +19,9 @@
         <div class="row justify-content-center">
         @foreach($data as $barang)
             <div class="col-md-3 mt-3">
-                <div class="flex" >
-                    <div class="card">
-                        <img src="{{ asset('storage/toko/'.$barang->gambar) }}" class="flex img-thumbnail card-img-top" alt="{{ $barang->nama_barang }}" style="width: 250px; height: 250px;">
+                <div class="d-flex" >
+                    <div class="card" style="width: 500px; ">
+                        <img src="{{ asset('storage/toko/'.$barang->gambar) }}" class="flex img-thumbnail card-img-top" alt="{{ $barang->nama_barang }}" width="900px" height="400px">
                         <div class="card-body text-center">
                             <div class="card-title">
                                 <h6><b>{{ $barang->nama_barang }}</b></h6>
